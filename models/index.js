6f5d47f7179a68f2eb2551739e1327b40671e2ed
Caitlin O'Reilly <coreilly29@gmail.com>
2022-10-23 23:05:39 -0400

    executed association methods on Sequelize methods to create relationships between them

--- models/index.js
+++ models/index.js
@@ -1,16 +1,30 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+// Products belongsTo Category (a category can have mult products but a product can only belong to one category)
+Product.belongsTo(Category, {
+  foreignKey: "product_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag) - allow products to have multiple tags & tags to have many products
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
